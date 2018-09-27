188318bc8b78aba692233e0206eadf6ec999653d
katunold <katunold94@gmail.com>
2018-09-28 00:11:12 +0300

    - Update database file with blacklist token table

--- Fast-food.sql
+++ Fast-food.sql
@@ -15,7 +15,13 @@ CREATE TABLE production."user" (
     password character varying(255) NOT NULL
 );
 
-DROP SCHEMA IF EXISTS tests CASCADE;
+CREATE TABLE production."blacklist_token" (
+    token_id SERIAL NOT NULL PRIMARY KEY,
+    token character varying(500) NOT NULL,
+    blacklisted_on timestamp(6) without time zone NOT NULL
+);
+
+DROP SCHEMA IF EXISTS test CASCADE;
 
 CREATE SCHEMA test;
 
@@ -26,6 +32,12 @@ CREATE TABLE test."user" (
     contact character varying(255) NOT NULL,
     user_type character varying(100) NOT NULL,
     password character varying(255) NOT NULL
+);
+
+CREATE TABLE test."blacklist_token" (
+    token_id SERIAL NOT NULL PRIMARY KEY,
+    token character varying(500) NOT NULL,
+    blacklisted_on timestamp(6) without time zone NOT NULL
 );
 
 
