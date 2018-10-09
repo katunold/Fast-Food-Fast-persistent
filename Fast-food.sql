3925cdbdb0eb384bd0a6cb89079ace0f7d245637
katunold <katunold94@gmail.com>
2018-10-09 16:00:44 +0300

    - Debugging sql

--- Fast-food.sql
+++ Fast-food.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS "Fast-Food";
 CREATE DATABASE "Fast-Food";
-\connect "Fast-Food"
+\connect "Fast-Food";
 
 DROP SCHEMA IF EXISTS production CASCADE;
 
@@ -21,6 +21,7 @@ CREATE TABLE production."menu_items" (
     item_name character varying(255) NOT NULL,
     user_id integer NOT NULL REFERENCES production."user"(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     item_status character varying(100) NOT NULL
+    price double precision NOT NULL
 );
 
 CREATE TABLE production.orders (
@@ -57,15 +58,15 @@ CREATE TABLE test."menu_items" (
     item_name character varying(255) NOT NULL,
     user_id integer NOT NULL REFERENCES test."user"(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     item_status character varying(100) NOT NULL
+    price double precision NOT NULL
 );
-
-CREATE TABLE test.orders (
+CREATE TABLE test."orders" (
     order_id SERIAL NOT NULL PRIMARY KEY,
-    user_id integer NOT NULL,
+    user_id integer NOT NULL REFERENCES test."user"(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     order_item character varying(255) NOT NULL,
     special_notes text NOT NULL,
     order_date timestamp(6) without time zone NOT NULL,
-    item_id integer NOT NULL,
+    item_id integer NOT NULL REFERENCES test."menu_items"(item_id) ON UPDATE CASCADE ON DELETE CASCADE,
     order_status character varying(100) NOT NULL
 );
 
