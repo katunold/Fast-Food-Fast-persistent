eb0ca9b74a6ad1ea93ad75d19ed1f3b9658e0fc6
katunold <katunold94@gmail.com>
2018-10-09 16:32:45 +0300

    - Debugging sql

--- Fast-food.sql
+++ Fast-food.sql
@@ -44,7 +44,7 @@ DROP SCHEMA IF EXISTS test CASCADE;
 
 CREATE SCHEMA test;
 
-CREATE TABLE test."user" (
+CREATE TABLE test.user (
     user_id SERIAL NOT NULL PRIMARY KEY,
     user_name character varying(255) NOT NULL,
     email character varying(255) NOT NULL,
@@ -53,24 +53,24 @@ CREATE TABLE test."user" (
     password character varying(255) NOT NULL
 );
 
-CREATE TABLE test."menu_items" (
+CREATE TABLE test.menu_items (
     item_id SERIAL NOT NULL,
     item_name character varying(255) NOT NULL,
-    user_id integer NOT NULL REFERENCES test."user"(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    user_id integer NOT NULL REFERENCES test.user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     item_status character varying(100) NOT NULL
     price double precision NOT NULL
 );
-CREATE TABLE test."orders" (
+CREATE TABLE test.orders (
     order_id SERIAL NOT NULL PRIMARY KEY,
-    user_id integer NOT NULL REFERENCES test."user"(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    user_id integer NOT NULL REFERENCES test.user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     order_item character varying(255) NOT NULL,
     special_notes text NOT NULL,
     order_date timestamp(6) without time zone NOT NULL,
-    item_id integer NOT NULL REFERENCES test."menu_items"(item_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    item_id integer NOT NULL REFERENCES test.menu_items(item_id) ON UPDATE CASCADE ON DELETE CASCADE,
     order_status character varying(100) NOT NULL
 );
 
-CREATE TABLE test."blacklist_token" (
+CREATE TABLE test.blacklist_token (
     token_id SERIAL NOT NULL PRIMARY KEY,
     token character varying(500) NOT NULL,
     blacklisted_on timestamp(6) without time zone NOT NULL
